24181cbdbcd934cfbcff1904108e338ee5226959
Kangmj46 <kmjae0406@naver.com>
2023-05-24 18:02:54 +0900

    modal fix

--- front-end/src/comp/Sidebar.js
+++ front-end/src/comp/Sidebar.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 import '../App.css';
 
 function Sidebar() {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
     return (
         <div className="sidebar">
             <div className="sidebarWrapper">
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Image Search</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem" href="#home">Home</li>
-                        <li className="sidebarListItem">How to do</li>
-                        <li className="sidebarListItem">Option</li>
+                        <React.Fragment>
+                            <li className="sidebarListItem"><a href="/home">Home</a></li>
+                            <li className="sidebarListItem">
+                                <React.Fragment>
+                                    <a onClick={openModal}>How to do</a>
+                                    <Modal open={modalOpen} close={closeModal} header="How to do image search">
+                                        <img className="eximg" src="/image-cropping-example"></img>
+                                    </Modal>
+                                </React.Fragment>
+                            </li>
+                            <li className="sidebarListItem">Option</li>
+                        </React.Fragment>
                     </ul>
                 </div>
             </div>
         </div>
-        )
+    )
 }
 
 export default Sidebar;
